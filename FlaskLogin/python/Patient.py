08b5a77e4aff719dad184f25b44e61d17318ebb2
jordangowdy11 <jordan.gowdy@mastek.com>
2019-02-13 10:30:41 +0000

    commit

---
+++ FlaskLogin/python/Patient.py
@@ -0,0 +1,9 @@
+'''
+Created on 13 Feb 2019
+
+@author: Jordan61077
+'''
+
+if __name__ == '__main__':
+    
+    pass
---
+++ FlaskLogin/python/data.py
@@ -0,0 +1,31 @@
+'''
+Created on 13 Feb 2019
+
+@author: Jordan61077
+'''
+def Areas():
+    areas = [
+        {
+            'id': 1,
+            'area': 'Leeds',
+            'postcode':'LS1 3EX'
+            
+        },
+        {
+            'id': 2,
+            'area': 'Bradford',
+            'postcode':'B31 3EX'
+            
+        },
+        {
+            'id': 3,
+            'area': 'Reading',
+            'postcode':'RS4 9AQ'
+            
+        }
+            
+    ] 
+    return areas
+
+    
+    
